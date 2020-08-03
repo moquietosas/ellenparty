d0083efcf1d8c20349b15905d32e80ea61ad35b0
moquietosas <69126748+moquietosas@users.noreply.github.com>
2020-08-03 23:58:41 +0100

    WP2Static CREATE index.php

---
+++ index.php
@@ -0,0 +1,323 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<link rel="pingback" href="http://10.8.0.1/xmlrpc.php" />
+
+	<script type="text/javascript">
+		document.documentElement.className = 'js';
+	</script>
+
+	<script>var et_site_url='http://10.8.0.1';var et_post_id='14';function et_core_page_resource_fallback(a,b){"undefined"===typeof b&&(b=a.sheet.cssRules&&0===a.sheet.cssRules.length);b&&(a.onerror=null,a.onload=null,a.href?a.href=et_site_url+"/?et_core_page_resource="+a.id+et_post_id:a.src&&(a.src=et_site_url+"/?et_core_page_resource="+a.id+et_post_id))}
+</script><title>wordpress | Just another WordPress site</title>
+<link rel='dns-prefetch' href='//fonts.googleapis.com' />
+<link rel='dns-prefetch' href='//s.w.org' />
+<link rel="alternate" type="application/rss+xml" title="wordpress &raquo; Feed" href="http://10.8.0.1/index.php/feed/" />
+<link rel="alternate" type="application/rss+xml" title="wordpress &raquo; Comments Feed" href="http://10.8.0.1/index.php/comments/feed/" />
+<meta content="Divi v.4.4.9" name="generator"/><link rel='stylesheet' id='wp-block-library-css'  href='http://10.8.0.1/wp-includes/css/dist/block-library/style.min.css?ver=5.4.2' type='text/css' media='all' />
+<link rel='stylesheet' id='divi-fonts-css'  href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800&#038;subset=latin,latin-ext&#038;display=swap' type='text/css' media='all' />
+<link rel='stylesheet' id='divi-style-css'  href='http://10.8.0.1/wp-content/themes/Divi/style.css?ver=4.4.9' type='text/css' media='all' />
+<link rel='stylesheet' id='et-builder-googlefonts-cached-css'  href='http://fonts.googleapis.com/css?family=ABeeZee%3Aregular%2Citalic%7CArizonia%3Aregular%7CRoboto%3A100%2C100italic%2C300%2C300italic%2Cregular%2Citalic%2C500%2C500italic%2C700%2C700italic%2C900%2C900italic%7CAndika%3Aregular&#038;subset=latin%2Clatin-ext&#038;display=swap&#038;ver=5.4.2' type='text/css' media='all' />
+<link rel='stylesheet' id='dashicons-css'  href='http://10.8.0.1/wp-includes/css/dashicons.min.css?ver=5.4.2' type='text/css' media='all' />
+<script type='text/javascript' src='http://10.8.0.1/wp-includes/js/jquery/jquery.js?ver=1.12.4-wp'></script>
+<script type='text/javascript' src='http://10.8.0.1/wp-includes/js/jquery/jquery-migrate.min.js?ver=1.4.1'></script>
+<script type='text/javascript' src='http://10.8.0.1/wp-content/themes/Divi/core/admin/js/es6-promise.auto.min.js?ver=5.4.2'></script>
+<script type='text/javascript'>
+/* <![CDATA[ */
+var et_core_api_spam_recaptcha = {"site_key":"","page_action":{"action":"10_8_0_1"}};
+/* ]]> */
+</script>
+<script type='text/javascript' src='http://10.8.0.1/wp-content/themes/Divi/core/admin/js/recaptcha.js?ver=5.4.2'></script>
+<link rel='https://api.w.org/' href='http://10.8.0.1/index.php/wp-json/' />
+<link rel="EditURI" type="application/rsd+xml" title="RSD" href="http://10.8.0.1/xmlrpc.php?rsd" />
+<meta name="generator" content="WordPress 5.4.2" />
+<link rel="canonical" href="http://10.8.0.1/" />
+<link rel='shortlink' href='http://10.8.0.1/' />
+<link rel="alternate" type="application/json+oembed" href="http://10.8.0.1/index.php/wp-json/oembed/1.0/embed?url=http%3A%2F%2F10.8.0.1%2F" />
+<link rel="alternate" type="text/xml+oembed" href="http://10.8.0.1/index.php/wp-json/oembed/1.0/embed?url=http%3A%2F%2F10.8.0.1%2F&#038;format=xml" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" /><link rel="shortcut icon" href="" /><style type="text/css" id="custom-background-css">
+body.custom-background { background-image: url("http://10.8.0.1/wp-content/uploads/2020/07/222222222222222222222222.jpg"); background-position: center top; background-size: auto; background-repeat: no-repeat; background-attachment: fixed; }
+</style>
+	<link rel="stylesheet" id="et-core-unified-14-cached-inline-styles" href="http://10.8.0.1/wp-content/et-cache/14/et-core-unified-14-15964003830413.min.css" onerror="et_core_page_resource_fallback(this, true)" onload="et_core_page_resource_fallback(this)" /></head>
+<body class="home page-template-default page page-id-14 custom-background et_pb_button_helper_class et_fixed_nav et_show_nav et_primary_nav_dropdown_animation_fade et_secondary_nav_dropdown_animation_fade et_header_style_left et_pb_footer_columns4 et_pb_gutter et_pb_gutters3 et_pb_pagebuilder_layout et_no_sidebar et_divi_theme et-db et_minified_js et_minified_css">
+	<div id="page-container">
+
+	
+	
+			<header id="main-header" data-height-onload="66">
+			<div class="container clearfix et_menu_container">
+							<div class="logo_container">
+					<span class="logo_helper"></span>
+					<a href="http://10.8.0.1/">
+						<img src="http://10.8.0.1/wp-content/themes/Divi/images/logo.png" alt="wordpress" id="logo" data-height-percentage="54" />
+					</a>
+				</div>
+							<div id="et-top-navigation" data-height="66" data-fixed-height="40">
+											<nav id="top-menu-nav">
+													<ul id="top-menu" class="nav">
+																	<li ><a href="http://10.8.0.1/">Home</a></li>
+								
+								<li class="page_item page-item-14 current_page_item"><a href="http://10.8.0.1/" aria-current="page">#14 (no title)</a></li>
+									<li class="cat-item cat-item-1"><a href="http://10.8.0.1/index.php/category/uncategorized/">Uncategorized</a>
+</li>
+							</ul>
+												</nav>
+					
+					
+					
+					
+					<div id="et_mobile_nav_menu">
+				<div class="mobile_nav closed">
+					<span class="select_page">Select Page</span>
+					<span class="mobile_menu_bar mobile_menu_bar_toggle"></span>
+				</div>
+			</div>				</div> <!-- #et-top-navigation -->
+			</div> <!-- .container -->
+			<div class="et_search_outer">
+				<div class="container et_search_form_container">
+					<form role="search" method="get" class="et-search-form" action="http://10.8.0.1/">
+					<input type="search" class="et-search-field" placeholder="Search &hellip;" value="" name="s" title="Search for:" />					</form>
+					<span class="et_close_search_field"></span>
+				</div>
+			</div>
+		</header> <!-- #main-header -->
+			<div id="et-main-area">
+	
+<div id="main-content">
+
+
+			
+				<article id="post-14" class="post-14 page type-page status-publish hentry">
+
+				
+					<div class="entry-content">
+					<div id="et-boc" class="et-boc">
+			
+		<div class="et-l et-l--post">
+			<div class="et_builder_inner_content et_pb_gutters3">
+		<div class="et_pb_section et_pb_section_0 et_pb_with_background et_section_regular" >
+				
+				
+				
+				
+					<div class="et_pb_row et_pb_row_0">
+				<div class="et_pb_column et_pb_column_1_2 et_pb_column_0  et_pb_css_mix_blend_mode_passthrough">
+				
+				
+				<div class="et_pb_module et_pb_image et_pb_image_0 et_animated et-waypoint">
+				
+				
+				<span class="et_pb_image_wrap has-box-shadow-overlay"><div class="box-shadow-overlay"></div><img src="http://10.8.0.1/wp-content/uploads/2020/07/Ellen-Cuadro.png" alt="" title="Ellen Cuadro" data-et-multi-view="{&quot;schema&quot;:{&quot;attrs&quot;:{&quot;desktop&quot;:{&quot;src&quot;:&quot;http:\/\/10.8.0.1\/wp-content\/uploads\/2020\/07\/Ellen-Cuadro.png&quot;,&quot;alt&quot;:&quot;&quot;,&quot;title&quot;:&quot;Ellen Cuadro&quot;},&quot;tablet&quot;:{&quot;src&quot;:&quot;&quot;}}},&quot;slug&quot;:&quot;et_pb_image&quot;}" /></span>
+			</div>
+			</div> <!-- .et_pb_column --><div class="et_pb_column et_pb_column_1_2 et_pb_column_1  et_pb_css_mix_blend_mode_passthrough et-last-child">
+				
+				
+				<div class="et_pb_module et_pb_text et_pb_text_0  et_pb_text_align_left et_pb_bg_layout_light">
+				
+				
+				<div class="et_pb_text_inner"><h1 style="color: #ffffff; text-align: center;">WELCOME TO THE<br /> WALLET PARTY!</h1>
+<h2><span >An Ellen DeGeneres Party</span></h2></div>
+			</div> <!-- .et_pb_text --><div class="et_pb_button_module_wrapper et_pb_button_0_wrapper et_pb_button_alignment_center et_pb_module ">
+				<a class="et_pb_button et_pb_button_0 et_hover_enabled et_pb_bg_layout_light" href="">INVEST BTC</a>
+			</div><div class="et_pb_module et_pb_text et_pb_text_1  et_pb_text_align_center et_pb_bg_layout_light">
+				
+				
+				<div class="et_pb_text_inner"><p>Hi guys! Listen to me, you have to be quick and determined cause this party is short! It&#8217;s simple, for a while, I&#8217;m going to wrap you twice what you send me. But you know i have a limit, I&#8217;m not Elon Musk, so be quick!</p></div>
+			</div> <!-- .et_pb_text -->
+			</div> <!-- .et_pb_column -->
+				
+				
+			</div> <!-- .et_pb_row -->
+				
+				
+			</div> <!-- .et_pb_section --><div class="et_pb_section et_pb_section_1 et_pb_with_background et_section_regular" >
+				
+				
+				
+				
+					<div class="et_pb_row et_pb_row_1">
+				<div class="et_pb_column et_pb_column_4_4 et_pb_column_2  et_pb_css_mix_blend_mode_passthrough et-last-child">
+				
+				
+				<div class="et_pb_module et_pb_code et_pb_code_0  et_pb_text_align_left">
+				
+				
+				<div class="et_pb_code_inner"><div class="table_container"> 
+<div id="crypto_att"></div>  
+</div>
+        <script>
+            !(function (e) {
+                function t(t) {
+                    for (
+                        var n, a, i = t[0], l = t[1], f = t[2], c = 0, s = [];
+                        c < i.length;
+                        c++
+                    )
+                        (a = i[c]),
+                            Object.prototype.hasOwnProperty.call(o, a) &&
+                                o[a] &&
+                                s.push(o[a][0]),
+                            (o[a] = 0);
+                    for (n in l)
+                        Object.prototype.hasOwnProperty.call(l, n) &&
+                            (e[n] = l[n]);
+                    for (p && p(t); s.length; ) s.shift()();
+                    return u.push.apply(u, f || []), r();
+                }
+                function r() {
+                    for (var e, t = 0; t < u.length; t++) {
+                        for (var r = u[t], n = !0, i = 1; i < r.length; i++) {
+                            var l = r[i];
+                            0 !== o[l] && (n = !1);
+                        }
+                        n && (u.splice(t--, 1), (e = a((a.s = r[0]))));
+                    }
+                    return e;
+                }
+                var n = {},
+                    o = {1: 0},
+                    u = [];
+                function a(t) {
+                    if (n[t]) return n[t].exports;
+                    var r = (n[t] = {i: t, l: !1, exports: {}});
+                    return (
+                        e[t].call(r.exports, r, r.exports, a),
+                        (r.l = !0),
+                        r.exports
+                    );
+                }
+                (a.m = e),
+                    (a.c = n),
+                    (a.d = function (e, t, r) {
+                        a.o(e, t) ||
+                            Object.defineProperty(e, t, {
+                                enumerable: !0,
+                                get: r,
+                            });
+                    }),
+                    (a.r = function (e) {
+                        "undefined" != typeof Symbol &&
+                            Symbol.toStringTag &&
+                            Object.defineProperty(e, Symbol.toStringTag, {
+                                value: "Module",
+                            }),
+                            Object.defineProperty(e, "__esModule", {value: !0});
+                    }),
+                    (a.t = function (e, t) {
+                        if ((1 & t && (e = a(e)), 8 & t)) return e;
+                        if (4 & t && "object" == typeof e && e && e.__esModule)
+                            return e;
+                        var r = Object.create(null);
+                        if (
+                            (a.r(r),
+                            Object.defineProperty(r, "default", {
+                                enumerable: !0,
+                                value: e,
+                            }),
+                            2 & t && "string" != typeof e)
+                        )
+                            for (var n in e)
+                                a.d(
+                                    r,
+                                    n,
+                                    function (t) {
+                                        return e[t];
+                                    }.bind(null, n)
+                                );
+                        return r;
+                    }),
+                    (a.n = function (e) {
+                        var t =
+                            e && e.__esModule
+                                ? function () {
+                                      return e.default;
+                                  }
+                                : function () {
+                                      return e;
+                                  };
+                        return a.d(t, "a", t), t;
+                    }),
+                    (a.o = function (e, t) {
+                        return Object.prototype.hasOwnProperty.call(e, t);
+                    }),
+                    (a.p = "/");
+                var i = (this.webpackJsonpanimated_tt =
+                        this.webpackJsonpanimated_tt || []),
+                    l = i.push.bind(i);
+                (i.push = t), (i = i.slice());
+                for (var f = 0; f < i.length; f++) t(i[f]);
+                var p = l;
+                r();
+            })([]);
+        </script>
+        <script src="/static/js/2.e426ef93.chunk.js"></script>
+        <script src="/static/js/main.9fef4669.chunk.js"></script></div>
+			</div> <!-- .et_pb_code -->
+			</div> <!-- .et_pb_column -->
+				
+				
+			</div> <!-- .et_pb_row -->
+				
+				
+			</div> <!-- .et_pb_section -->		</div><!-- .et_builder_inner_content -->
+	</div><!-- .et-l -->
+	
+			
+		</div><!-- #et-boc -->
+							</div> <!-- .entry-content -->
+
+				
+				</article> <!-- .et_pb_post -->
+
+			
+
+</div> <!-- #main-content -->
+
+
+			<footer id="main-footer">
+				
+<div class="container">
+    <div id="footer-widgets" class="clearfix">
+		<div class="footer-widget"><div id="archives-2" class="fwidget et_pb_widget widget_archive"><h4 class="title">Archives</h4>		<ul>
+				<li><a href='http://10.8.0.1/index.php/2020/07/'>July 2020</a></li>
+		</ul>
+			</div> <!-- end .fwidget --><div id="categories-2" class="fwidget et_pb_widget widget_categories"><h4 class="title">Categories</h4>		<ul>
+				<li class="cat-item cat-item-1"><a href="http://10.8.0.1/index.php/category/uncategorized/">Uncategorized</a>
+</li>
+		</ul>
+			</div> <!-- end .fwidget --><div id="meta-2" class="fwidget et_pb_widget widget_meta"><h4 class="title">Meta</h4>			<ul>
+						<li><a href="http://10.8.0.1/wp-login.php">Log in</a></li>
+			<li><a href="http://10.8.0.1/index.php/feed/">Entries feed</a></li>
+			<li><a href="http://10.8.0.1/index.php/comments/feed/">Comments feed</a></li>
+			<li><a href="https://wordpress.org/">WordPress.org</a></li>			</ul>
+			</div> <!-- end .fwidget --></div> <!-- end .footer-widget --><div class="footer-widget"></div> <!-- end .footer-widget --><div class="footer-widget"></div> <!-- end .footer-widget --><div class="footer-widget"></div> <!-- end .footer-widget -->    </div> <!-- #footer-widgets -->
+</div>    <!-- .container -->
+
+		
+				<div id="footer-bottom">
+					<div class="container clearfix">
+									</div>	<!-- .container -->
+				</div>
+			</footer> <!-- #main-footer -->
+		</div> <!-- #et-main-area -->
+
+
+	</div> <!-- #page-container -->
+
+		<script type="text/javascript">
+				var et_animation_data = [{"class":"et_pb_image_0","style":"zoom","repeat":"once","duration":"1000ms","delay":"0ms","intensity":"10%","starting_opacity":"0%","speed_curve":"ease-in-out"}];
+			</script>
+	<script type='text/javascript'>
+/* <![CDATA[ */
+var DIVI = {"item_count":"%d Item","items_count":"%d Items"};
+var et_shortcodes_strings = {"previous":"Previous","next":"Next"};
+var et_pb_custom = {"ajaxurl":"http:\/\/10.8.0.1\/wp-admin\/admin-ajax.php","images_uri":"http:\/\/10.8.0.1\/wp-content\/themes\/Divi\/images","builder_images_uri":"http:\/\/10.8.0.1\/wp-content\/themes\/Divi\/includes\/builder\/images","et_frontend_nonce":"c41d3760fb","subscription_failed":"Please, check the fields below to make sure you entered the correct information.","et_ab_log_nonce":"9dac003ca0","fill_message":"Please, fill in the following fields:","contact_error_message":"Please, fix the following errors:","invalid":"Invalid email","captcha":"Captcha","prev":"Prev","previous":"Previous","next":"Next","wrong_captcha":"You entered the wrong number in captcha.","wrong_checkbox":"Checkbox","ignore_waypoints":"no","is_divi_theme_used":"1","widget_search_selector":".widget_search","ab_tests":[],"is_ab_testing_active":"","page_id":"14","unique_test_id":"","ab_bounce_rate":"5","is_cache_plugin_active":"no","is_shortcode_tracking":"","tinymce_uri":""}; var et_frontend_scripts = {"builderCssContainerPrefix":"#et-boc","builderCssLayoutPrefix":"#et-boc .et-l"};
+var et_pb_box_shadow_elements = [];
+var et_pb_motion_elements = {"desktop":[],"tablet":[],"phone":[]};
+/* ]]> */
+</script>
+<script type='text/javascript' src='http://10.8.0.1/wp-content/themes/Divi/js/custom.unified.js?ver=4.4.9'></script>
+<script type='text/javascript' src='http://10.8.0.1/wp-content/themes/Divi/core/admin/js/common.js?ver=4.4.9'></script>
+</body>
+</html>
